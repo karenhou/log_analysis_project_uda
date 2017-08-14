74b5c480070fe4b0e34d0eb79c381764e969bf8f
Karen Hou <ih2g10@gmail.com>
2017-08-15 00:06:04 +0900

    add get_query_results to handle all the query

--- newsdatadb.py
+++ newsdatadb.py
@@ -6,6 +6,7 @@ import psycopg2
 
 DBNAME = "news"
 
+
 def connect(database_name):
     """Connect to the PostgreSQL database.  Returns a database connection."""
     try:
@@ -16,44 +17,45 @@ def connect(database_name):
         print "Unable to connect to database"
         raise e
 
-# list top 3 most viewed articles
+
+def get_query_results(query):
+    # connect to database
+    db, c = connect(DBNAME)
+    # pass in the query
+    c.execute(query)
+    rows = c.fetchall()
+    db.close()
+    # return the results of query
+    return rows
+
+
 def list_most_pop_articles():
-    # connect to database news
-    db, c = connect(DBNAME)
-    # send sql cmd
-    c.execute("select title, views_num from total_views order by views_num desc limit 3")  # NOQA
-    rows = c.fetchall()
-    # close database connection
-    db.close()
+    # list top 3 most viewed articles
+    results = get_query_results(
+        "select title, views_num from total_views order by views_num desc limit 3")  # NOQA
     print("What are the most popular three articles of all time?")
-    for articles, views in rows:
+    for articles, views in results:
         print(str(articles) + "--" + str(views) + " views")
-    return rows
 
-# Rank arthors by views of their articles
+
 def list_most_pop_authors():
-    db, c = connect(DBNAME)
-    c.execute("select name, sum(views_num) as author_total from rank_author group by rank_author.name order by author_total desc")  # NOQA
-    rows = c.fetchall()
-    db.close()
+    # Rank arthors by views of their articles
+    results = get_query_results(
+        "select name, sum(views_num) as author_total from rank_author group by rank_author.name order by author_total desc")  # NOQA
     print("Who are the most popular article authors of all time?")
-    for author, views in rows:
+    for author, views in results:
         print(str(author) + "--" + str(views) + " views")
-    return rows
 
-# list the days that has more than 1% of https request errors
+
 def list_http_errors():
-    db, c = connect(DBNAME)
-    c.execute("select * from result_table where result > 0.01")
-    rows = c.fetchall()
-    db.close()
+    # list the days that has more than 1% of https request errors
+    results = get_query_results(
+        "select * from result_table where result > 0.01")
     print("On which days did more than 1% of requests lead to errors?")
-    for date, error in rows:
+    for date, error in results:
         print(str(date) + "--" + '{:.1f}'.format(error * 100) + '% errors')
-    return rows
 
 if __name__ == '__main__':
     list_most_pop_articles()
     list_most_pop_authors()
     list_http_errors()
-
